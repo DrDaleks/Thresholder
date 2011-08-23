3c96e55c63f77e11534cf7a00465de89b077ca43
Alexandre Dufour <adufour@pasteur.fr>
2011-08-23 14:37:11 +0200

    first commit (v.2.0.4.1)

---
+++ src/plugins/adufour/thresholder/KMeans.java
@@ -0,0 +1,174 @@
+package plugins.adufour.thresholder;
+
+import icy.sequence.Sequence;
+import icy.type.collection.array.Array1DUtil;
+
+public class KMeans
+{
+	public static final int	DEFAULT_KMEANS_BINS	= 255;
+	
+	/**
+	 * Calculates the optimal thresholds on the input data for the given number of classes and
+	 * default bins size
+	 * 
+	 * @param input
+	 *            the input sequence
+	 * @param nbClasses
+	 *            the number of classes to extract
+	 * @return an array of thresholds of size [input.getSizeC()][nbClasses-1]
+	 */
+	public static double[][] computeKMeansThresholds(Sequence input, int nbClasses)
+	{
+		return computeKMeansThresholds(input, nbClasses, DEFAULT_KMEANS_BINS);
+	}
+	
+	/**
+	 * Calculates the optimal thresholds on the input data for the given number of classes and bins
+	 * size
+	 * 
+	 * @param input
+	 *            the input sequence
+	 * @param nbClasses
+	 *            the number of classes to extract
+	 * @param binPrecision
+	 *            the size of the histogram bins (higher is slower but more precise)
+	 * @return an array of thresholds of size [input.getSizeC()][nbClasses-1]
+	 */
+	public static double[][] computeKMeansThresholds(Sequence input, int nbClasses, int binPrecision)
+	{
+		double[][] thrs = new double[input.getSizeC()][];
+		
+		for (int c = 0; c < input.getSizeC(); c++)
+			thrs[c] = computeKMeansThresholds(input, c, nbClasses, binPrecision);
+		
+		return thrs;
+	}
+	
+	/**
+	 * Calculates the optimal thresholds on the specified channel of the input data for the given
+	 * number of classes and bins size
+	 * 
+	 * @param input
+	 *            the input sequence
+	 * @param c
+	 *            the channel on which to compute the threshold(s)
+	 * @param nbClasses
+	 *            the number of classes to extract
+	 * @param binPrecision
+	 *            the size of the histogram bins (higher is slower but more precise)
+	 * @return an array of thresholds for the given channel, of size [nbClasses-1]
+	 */
+	public static double[] computeKMeansThresholds(Sequence input, int c, int nbClasses, int binPrecision)
+	{
+		double[] thresholds = new double[nbClasses - 1];
+		
+		input.updateComponentsBounds(true, true);
+		double[] minmax = input.getComponentAbsBounds(c);
+		double min = minmax[0], max = minmax[1];
+		double fact = (binPrecision - 1) / (max - min);
+		double[] histo = new double[binPrecision];
+		
+		// histogram computation
+		{
+			double[] sliceXY = new double[input.getSizeX() * input.getSizeY()];
+			
+			for (int t = 0; t < input.getSizeT(); t++)
+				for (int z = 0; z < input.getSizeZ(); z++)
+				{
+					Array1DUtil.arrayToDoubleArray(input.getDataXY(t, z, c), sliceXY, false);
+					for (double d : sliceXY)
+						histo[(int) ((d - min) * fact)]++;
+				}
+		}
+		
+		int[] centers = kMeans_Histogram1D(histo, nbClasses);
+		
+		// Compute thresholds between class centers
+		
+		java.util.Arrays.sort(centers);
+		for (int k = 1; k < nbClasses; k++)
+		{
+			thresholds[k - 1] = min + (centers[k - 1] + (centers[k] - centers[k - 1]) / 2.0) / fact;
+		}
+		
+		return thresholds;
+	}
+	
+	/**
+	 * KMeans classification algorithm, optimized for 1D histogram data. The algorithm is
+	 * initialized by spacing the class centers equally
+	 * 
+	 * @param histogram
+	 *            the histogram to classify
+	 * @param nbClasses
+	 *            the number of classes to extract
+	 * @return the optimal class centers after convergence
+	 */
+	public static int[] kMeans_Histogram1D(double[] histogram, int nbClasses)
+	{
+		int[] centers = new int[nbClasses];
+		double[] sums = new double[nbClasses];
+		double[] nbElements = new double[nbClasses];
+		
+		// basic class initialization : regularly divide the space
+		
+		for (int i = 0; i < nbClasses; i++)
+		{
+			centers[i] = (int) ((histogram.length - 1f) * (i + 1f) / (nbClasses + 1f));
+		}
+		
+		// main loop
+		
+		boolean convergence = false;
+		
+		while (!convergence)
+		{
+			// assume the convergence is reached
+			// (invalidate this assumption later if class means move)
+			convergence = true;
+			
+			java.util.Arrays.fill(nbElements, 0);
+			java.util.Arrays.fill(sums, 0);
+			
+			for (int i = 0; i < histogram.length; i++)
+			{
+				int closestClass = 0;
+				double minDistance = Double.MAX_VALUE;
+				
+				// compute the shortest distance from the current bin
+				// to a class center and assign the bin to that class
+				for (int k = 0; k < nbClasses; k++)
+				{
+					double distance = Math.abs(i - centers[k]);
+					
+					if (distance < minDistance)
+					{
+						minDistance = distance;
+						closestClass = k;
+					}
+				}
+				
+				// to update each class center to the mean,
+				// accumulate both number and sum per class
+				double nbElemInCurrentBin = histogram[i];
+				sums[closestClass] += i * nbElemInCurrentBin;
+				nbElements[closestClass] += nbElemInCurrentBin;
+			}
+			
+			// once all bins have been assigned to a class,
+			// the class centers can be moved toward the new means
+			
+			for (int k = 0; k < nbClasses; k++)
+			{
+				int oldCenter = centers[k];
+				int newCenter = (int) (sums[k] / nbElements[k]);
+				
+				convergence &= (oldCenter == newCenter);
+				
+				centers[k] = newCenter;
+			}
+		}
+		
+		return centers;
+	}
+}
---
+++ src/plugins/adufour/thresholder/Thresholder.java
@@ -0,0 +1,207 @@
+package plugins.adufour.thresholder;
+
+import icy.image.IcyBufferedImage;
+import icy.sequence.Sequence;
+import icy.type.collection.array.Array1DUtil;
+import plugins.adufour.ezplug.EzPlug;
+import plugins.adufour.ezplug.EzVar;
+import plugins.adufour.ezplug.EzVarBoolean;
+import plugins.adufour.ezplug.EzVarDoubleArray;
+import plugins.adufour.ezplug.EzVarEnum;
+import plugins.adufour.ezplug.EzVarInteger;
+import plugins.adufour.ezplug.EzVarListener;
+import plugins.adufour.ezplug.EzVarSequence;
+
+public class Thresholder extends EzPlug
+{
+	private enum ThresholdMethod
+	{
+		MANUAL, K_MEANS
+	}
+	
+	private EzVarSequence				in			= new EzVarSequence("Input");
+	private EzVarInteger				channel		= new EzVarInteger("Channel", 0, 0, 1, 1);
+	private EzVarBoolean				inPlace		= new EzVarBoolean("In-place", false);
+	private EzVarEnum<ThresholdMethod>	method		= new EzVarEnum<ThresholdMethod>("Method", ThresholdMethod.values());
+	private EzVarInteger				nbClasses	= new EzVarInteger("K-means classes", 2, KMeans.DEFAULT_KMEANS_BINS, 1);
+	private EzVarDoubleArray			thresholds	= new EzVarDoubleArray("Manual thresholds", new Double[][] {}, true);
+	
+	@Override
+	public void initialize()
+	{
+		super.addEzComponent(in);
+		super.addEzComponent(channel);
+		
+		in.addVarChangeListener(new EzVarListener<Sequence>()
+		{
+			@Override
+			public void variableChanged(EzVar<Sequence> source, Sequence newValue)
+			{
+				if (newValue == null)
+				{
+					channel.setVisible(false);
+				}
+				else
+				{
+					int sizeC = newValue.getSizeC();
+					switch (sizeC)
+					{
+						case 1:
+						{
+							channel.setVisible(false);
+							channel.setValue(0);
+							break;
+						}
+						default:
+						{
+							channel.setVisible(true);
+							channel.setMaxValue(sizeC - 1);
+						}
+					}
+				}
+			}
+		});
+		
+		super.addEzComponent(inPlace);
+		
+		super.addEzComponent(method);
+		
+		super.addEzComponent(nbClasses);
+		method.addVisibilityTriggerTo(nbClasses, ThresholdMethod.K_MEANS);
+		
+		super.addEzComponent(thresholds);
+		method.addVisibilityTriggerTo(thresholds, ThresholdMethod.MANUAL);
+		
+		super.setTimeDisplay(true);
+	}
+	
+	@Override
+	public void execute()
+	{
+		Sequence inSeq = in.getValue();
+		
+		int c = channel.getValue();
+		
+		ThresholdMethod algorithm = method.getValue();
+		
+		double[] _thrs = null;
+		
+		switch (method.getValue())
+		{
+			case MANUAL:
+			{
+				Double[] thrs = thresholds.getValue();
+				_thrs = new double[thrs.length];
+				for (int i = 0; i < thrs.length; i++)
+					_thrs[i] = thrs[i].doubleValue();
+				
+				break;
+			}
+			case K_MEANS:
+			{
+				_thrs = KMeans.computeKMeansThresholds(inSeq, c, nbClasses.getValue().shortValue(), KMeans.DEFAULT_KMEANS_BINS);
+				break;
+			}
+			default:
+				throw new UnsupportedOperationException(algorithm + " method");
+		}
+		
+		Sequence s = threshold(inSeq, c, _thrs, inPlace.getValue());
+		
+		if (inPlace.getValue())
+			s.dataChanged();
+		else addSequence(s);
+		
+	}
+	
+	/**
+	 * Threshold the given sequence channel with the specified thresholds, and returns the result as
+	 * a labeled sequence.<br>
+	 * Note: thresholds are inclusive: values equal to a threshold are considered as "above"
+	 * 
+	 * @param input
+	 * @param c
+	 * @param thresholds
+	 * @param inPlace
+	 * @return
+	 */
+	public static Sequence threshold(Sequence input, int c, double[] thresholds, boolean inPlace)
+	{
+		Sequence output = inPlace ? input : new Sequence();
+		
+		int dataType = input.getDataType();
+		boolean signed = input.isSignedDataType();
+		
+		double thr0 = thresholds[0];
+		int maxThresholdIndex = thresholds.length - 1;
+		int length = input.getSizeX() * input.getSizeY();
+		
+		output.beginUpdate();
+		
+		for (int t = 0; t < input.getSizeT(); t++)
+		{
+			for (int z = 0; z < input.getSizeZ(); z++)
+			{
+				Object _in2D = input.getDataXY(t, z, c);
+				
+				IcyBufferedImage outSlice;
+				
+				if (inPlace)
+				{
+					outSlice = input.getImage(t, z);
+				}
+				else
+				{
+					outSlice = new IcyBufferedImage(input.getSizeX(), input.getSizeY(), 1, dataType);
+					output.setImage(t, z, outSlice);
+				}
+				
+				Object _out2D = outSlice.getDataXY(inPlace ? c : 0);
+				
+				withTheNextPixel: for (int i = 0; i < length; i++)
+				{
+					double val = Array1DUtil.getValue(_in2D, i, dataType, signed);
+					
+					// background
+					if (val < thr0)
+					{
+						if (inPlace)
+						{
+							Array1DUtil.setValue(_out2D, i, dataType, 0);
+						}
+						continue withTheNextPixel;
+					}
+					// special 2-class case
+					if (maxThresholdIndex == 0)
+					{
+						Array1DUtil.setValue(_out2D, i, dataType, 1);
+						continue withTheNextPixel;
+					}
+					
+					// default n-class case
+					
+					// browse thresholds from highest to lowest (above 1 to save one test)
+					// assign the first positive match and break the loop
+					for (int thr = maxThresholdIndex; thr > 0; thr--)
+						if (val >= thresholds[thr])
+						{
+							Array1DUtil.setValue(_out2D, i, dataType, thr + 1);
+							continue withTheNextPixel;
+						}
+					
+					// last possible case: class 1
+					Array1DUtil.setValue(_out2D, i, dataType, 1);
+				}
+			}
+		}
+		
+		output.endUpdate();
+		
+		return output;
+	}
+	
+	public void clean()
+	{
+	}
+	
+}
